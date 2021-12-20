58370d0da99cdec1abccd8ae218f18f151d43b30
Herrler <rainer.herrler@fhws.de>
2021-12-20 21:21:53 +0100

    First commit of Hello World

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Prof Dr. Rainer Herrler
+// Version     :
+// Copyright   : (c) 2021
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
