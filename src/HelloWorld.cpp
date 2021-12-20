1c1c7388b782b09e518022e3dd2adb259c823d9f
herrler <herrler@FE-N319.fhws.de>
2021-12-20 21:36:10 +0100

    Added Christmas Tree Implementation

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -9,7 +9,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main(){
+    char cTree;
+    int iSize, iSpace;
+
+    std::cout << "Christmas Tree Application" << std::endl;
+    std::cout << "Enter the size of your christmas tree: ";
+    std::cin >> iSize;
+    std::cout << "Enter the character you would like to use for your tree: ";
+    std::cin >> cTree;
+
+    iSpace = iSize;
+
+    for (int i = 0; i < iSize*2; i+=2){
+        std::string sTree(i, cTree);
+        std::string sSpace(iSpace, ' ');
+        std::cout << sSpace + sTree + sSpace<< std::endl;
+        iSpace -= 1;
+    }
+
+    return 0;
 }
